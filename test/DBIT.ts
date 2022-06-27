b7408c7998c3290afc3f513eb92355c4ea945c71
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-06-27 13:57:12 +0200

    WIP: refactoring

--- test/DBIT.ts
+++ test/DBIT.ts
@@ -33,8 +33,8 @@ contract("DBIT token", async (accounts: any) => {
     });
 
 
-    it.only('is able to mint the collateralised supply via bank', async () => {
-        const amt = web3.utils.toWei('100','wei');
+    it('is able to mint the collateralised supply via bank', async () => {
+        const amt = web3.utils.toNumber('100');
         await dbitObj.mintCollateralisedSupply(user2, amt , {from: bankAddress});
        expect( web3.utils.toWei(await dbitObj.getCollateralisedBalance(user2),'wei')).to.equal(amt);
     });
@@ -86,9 +86,7 @@ contract("DBIT token", async (accounts: any) => {
 
     });
 
-    it(' directTransfer to another address ', async () => {
-
-
+    it('directTransfer to another address ', async () => {
         await dbitObj.mintCollateralisedSupply(bankAddress, 300, {from: bankAddress});
         await dbitObj.mintCollateralisedSupply(airdropAddress, 300, {from: airdropAddress});
         await dbitObj.mintAirdropSupply(bankAddress, 1000, {from: bankAddress});
@@ -97,14 +95,49 @@ contract("DBIT token", async (accounts: any) => {
     });
 
 
-    it('transfer is working between any general address', async () => {
-        await dbitObj.mintCollateralisedSupply(User1, 300, {from: bankAddress});
-        await dbitObj.mintCollateralisedSupply(user2, 900, {from: bankAddress})
-        await dbitObj.mintAllocatedSupply(User1, 300, {from: governance});
-        await dbitObj.mintAllocatedSupply(user2, 800, {from: governance})
-         await dbitObj.transferFrom(User1, user2, {from: governance});
+    it('gets correct locked Supply', async () => {
+        //await dbitObj
 
+        await dbitObj.mintCollateralisedSupply(User1, '300', {from: bankAddress});
+        await dbitObj.mintCollateralisedSupply(user2, '900', {from: bankAddress});
+        await dbitObj.mintAllocatedSupply(User1, '300', {from: governance});
+        await dbitObj.mintAllocatedSupply(user2, '800', {from: governance});
+        await dbitObj.mintAirdropSupply(User1, '100', {from:airdropAddress});
+        await dbitObj.mintAirdropSupply(user2, '100', {from:airdropAddress});
+        const lockedBalanceUser1 = await dbitObj.getLockedBalance(User1,{from: User1});
+        const lockedBalanceUser2 = await dbitObj.getAirdropBalance(user2, {from:user2});
+        console.log('locked bal of  user1',web3.utils.toNumber(lockedBalanceUser1));
+        console.log('locked bal of  user2',web3.utils.toNumber(lockedBalanceUser2));
 
     });
 
+    it('transfer is working between any general address', async () => {
+        await dbitObj.mintCollateralisedSupply(User1, '300', {from: bankAddress});
+        await dbitObj.mintCollateralisedSupply(user2, '900', {from: bankAddress});
+        await dbitObj.mintAllocatedSupply(User1, '300', {from: governance});
+        await dbitObj.mintAllocatedSupply(user2, '800', {from: governance});
+        await dbitObj.mintAirdropSupply(User1, '100', {from:airdropAddress});
+        await dbitObj.mintAirdropSupply(user2, '100', {from:airdropAddress});
+        
+        const lockedBalanceUser1 = await dbitObj.getLockedBalance(User1,{from: User1});
+        const lockedBalanceUser2 = await dbitObj.getAirdropBalance(user2, {from:user2});
+        console.log('locked bal of  user1',web3.utils.toNumber(lockedBalanceUser1));
+        console.log('locked bal of  user2',web3.utils.toNumber(lockedBalanceUser2));
+        console.log('total bal of User1',web3.utils.toNumber(await dbitObj.balanceOf(User1)));
+        console.log('total bal of User1',web3.utils.toNumber(await dbitObj.balanceOf(user2)));
+        await dbitObj.transfer(user2,'20', {from: User1});
+        expect(web3.utils.toNumber(await dbitObj.balanceOf(user2))).to.greaterThan(web3.utils.toNumber(1800));
+        expect(web3.utils.toNumber(await dbitObj.balanceOf(User1))).to.lessThan(web3.utils.toNumber(700));
+    });
+
+    it('direct transfer should work correctly to transfer between exchange and bank', async () => {
+        await dbitObj.mintCollateralisedSupply(bankAddress, '300', {from: bankAddress});
+        await dbitObj.mintCollateralisedSupply(exchangeAddress, '900', {from: bankAddress});
+        
+        await dbitObj.directTransfer(exchangeAddress, '100', {from: bankAddress});
+        console.log('bal for the bankAddress: ' + await dbitObj.balanceOf(exchangeAddress));
+        console.log('bal for exchangeAddress: ' + await dbitObj.balanceOf(bankAddress));
+        expect(web3.utils.toNumber(await dbitObj.balanceOf(exchangeAddress))).to.greaterThan(web3.utils.toNumber(1000));
+        expect(web3.utils.toNumber(await dbitObj.balanceOf(bankAddress))).to.lessThan(web3.utils.toNumber(web3.utils.toNumber(200)));
+    });
 })
